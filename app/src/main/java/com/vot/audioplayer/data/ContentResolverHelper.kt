4bedeaccdb6eafbb28c589b4ed157c1229dccc64
손소독 <vot768@naver.com>
2022-04-03 22:46:17 +0900

    1. 환경 설정 및 Audio data class 정의

---
+++ app/src/main/java/com/vot/audioplayer/data/ContentResolverHelper.kt
@@ -0,0 +1,8 @@
+package com.vot.audioplayer.data
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class ContentResolverHelper @Inject constructor(@ApplicationContext val context: Context) {
+}
