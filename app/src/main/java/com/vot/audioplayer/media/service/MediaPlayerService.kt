6e7e08420b40508f84a420e1d55c3b4fee218e4b
손소독 <vot768@naver.com>
2022-04-10 16:52:53 +0900

    1. 미디어 및 브라우저 서비스 구성

--- app/src/main/java/com/vot/audioplayer/media/service/MediaPlayerService.kt
+++ app/src/main/java/com/vot/audioplayer/media/service/MediaPlayerService.kt
@@ -1,20 +1,49 @@
 package com.vot.audioplayer.media.service
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.MediaDescription
+import android.net.Uri
 import android.os.Bundle
+import android.os.ResultReceiver
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaMetadata
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.ext.mediasession.TimelineQueueNavigator
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import com.vot.audioplayer.R
 import com.vot.audioplayer.media.constants.S
+import com.vot.audioplayer.media.exoplayer.MediaPlayerNotificationManager
 import com.vot.audioplayer.media.exoplayer.MediaSource
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MediaPlayerService : MediaBrowserServiceCompat() {
+
+    companion object {
+        const val TAG: String = "MediaPlayerService"
+
+        var currentDuration: Long = 0L
+            private set
+    }
 
     @Inject
     lateinit var dataSourceFactory: CacheDataSource.Factory
@@ -28,6 +57,38 @@ class MediaPlayerService : MediaBrowserServiceCompat() {
     private val serviceJob = SupervisorJob()
     private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+    private lateinit var mediaPlayerNotificationManager: MediaPlayerNotificationManager
+
+    private var currentPlayingMedia: MediaMetadataCompat? = null
+    private val isPlayerInitialized: Boolean = false
+    var isForegroundService: Boolean = false
+
+    override fun onCreate() {
+        super.onCreate()
+        val sessionActivityIntent = packageManager
+            ?.getLaunchIntentForPackage(packageName)
+            ?.let { sessionIntent ->
+                PendingIntent.getActivity(
+                    this,
+                    0,
+                    sessionIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or
+                            PendingIntent.FLAG_IMMUTABLE
+                )
+            }
+        mediaSession = MediaSessionCompat(this, TAG).apply {
+            setSessionActivity(sessionActivityIntent)
+            isActive = true
+        }
+
+        sessionToken = mediaSession.sessionToken
+
+        mediaSessionConnector = MediaSessionConnector(mediaSession).apply {
+        }
+    }
+
     override fun onGetRoot(
         clientPackageName: String,
         clientUid: Int,
@@ -40,7 +101,188 @@ class MediaPlayerService : MediaBrowserServiceCompat() {
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
-        TODO("Not yet implemented")
+        when (parentId) {
+            S.MEDIA_ROOT_ID -> {
+                val resultsSent = mediaSource.whenReady { isInitialized ->
+                    if (isInitialized) {
+                        result.sendResult(mediaSource.asMediaItem())
+                    } else {
+                        result.sendResult(null)
+                    }
+                }
+                if (!resultsSent) {
+                    result.detach()
+                }
+            }
+            else -> Unit
+        }
+    }
+
+    override fun onCustomAction(action: String, extras: Bundle?, result: Result<Bundle>) {
+        when (action) {
+            S.START_MEDIA_PLAY_ACTION -> {
+                mediaPlayerNotificationManager.showNotification(exoPlayer)
+            }
+            S.REFRESH_MEDIA_PLAY_ACTION -> {
+                mediaSource.refresh()
+                notifyChildrenChanged(S.MEDIA_ROOT_ID)
+            }
+            else -> Unit
+        }
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        exoPlayer.stop()
+        exoPlayer.clearMediaItems()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+        exoPlayer.release()
+    }
+
+    inner class PlayerNotificationListener : PlayerNotificationManager.NotificationListener {
+
+        override fun onNotificationCancelled(
+            notificationId: Int,
+            dismissedByUser: Boolean
+        ) {
+            stopForeground(true)
+            isForegroundService = false
+            stopSelf()
+        }
+
+        override fun onNotificationPosted(
+            notificationId: Int,
+            notification: Notification,
+            ongoing: Boolean
+        ) {
+            if (ongoing && !isForegroundService) {
+                ContextCompat.startForegroundService(
+                    applicationContext,
+                    Intent(
+                        applicationContext,
+                        this@MediaPlayerService.javaClass
+                    )
+                )
+                startForeground(notificationId, notification)
+                isForegroundService = true
+            }
+        }
+
+    }
+
+    inner class AudioMediaPlayBackPreparer : MediaSessionConnector.PlaybackPreparer {
+        override fun onCommand(
+            player: Player,
+            command: String,
+            extras: Bundle?,
+            cb: ResultReceiver?
+        ): Boolean {
+            return false
+        }
+
+        override fun getSupportedPrepareActions(): Long =
+            PlaybackStateCompat.ACTION_PREPARE_FROM_MEDIA_ID or
+                    PlaybackStateCompat.ACTION_PLAY_FROM_MEDIA_ID
+
+        override fun onPrepare(playWhenReady: Boolean) = Unit
+
+        override fun onPrepareFromMediaId(
+            mediaId: String,
+            playWhenReady: Boolean,
+            extras: Bundle?
+        ) {
+            mediaSource.whenReady {
+                val itemToPlay = mediaSource.audioMediaMetaData.find {
+                    it.description.mediaId == mediaId
+                }
+
+                currentPlayingMedia = itemToPlay
+
+                preparePlayer(
+                    mediaMetadata = mediaSource.audioMediaMetaData,
+                    itemToPlay = itemToPlay,
+                    playWhenReady = playWhenReady
+                )
+            }
+        }
+
+        override fun onPrepareFromSearch(
+            query: String,
+            playWhenReady:
+            Boolean, extras: Bundle?
+        ) = Unit
+
+        override fun onPrepareFromUri(
+            uri: Uri,
+            playWhenReady: Boolean,
+            extras: Bundle?
+        ) = Unit
+
+    }
+
+    inner class MediaQueueNavigator(
+        mediaSessionCompat: MediaSessionCompat
+    ) : TimelineQueueNavigator(mediaSessionCompat) {
+
+        override fun getMediaDescription(
+            player: Player,
+            windowIndex: Int
+        ): MediaDescriptionCompat {
+            if (windowIndex < mediaSource.audioMediaMetaData.size) {
+                return mediaSource.audioMediaMetaData[windowIndex].description
+            }
+            return MediaDescriptionCompat.Builder().build()
+        }
+
+    }
+
+    private fun preparePlayer(
+        mediaMetadata: List<MediaMetadataCompat>,
+        itemToPlay: MediaMetadataCompat?,
+        playWhenReady: Boolean
+    ) {
+        val indexToPlay = if (currentPlayingMedia == null) 0 else mediaMetadata.indexOf(itemToPlay)
+
+    }
+
+    private inner class PlayerEventListener : Player.Listener {
+
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            when (playbackState) {
+                Player.STATE_BUFFERING, Player.STATE_READY -> {
+                    mediaPlayerNotificationManager
+                        .showNotification(exoPlayer)
+                }
+                else -> {
+                    mediaPlayerNotificationManager
+                        .hideNotification()
+                }
+            }
+        }
+
+        override fun onEvents(player: Player, events: Player.Events) {
+            super.onEvents(player, events)
+            currentDuration = player.duration
+        }
+
+        override fun onPlayerError(error: PlaybackException) {
+            var message = R.string.generic_error
+
+            if (error.errorCode == PlaybackException.ERROR_CODE_IO_FILE_NOT_FOUND) {
+                message = R.string.error_media_not_found
+            }
+
+            Toast.makeText(
+                applicationContext,
+                message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 
 }
