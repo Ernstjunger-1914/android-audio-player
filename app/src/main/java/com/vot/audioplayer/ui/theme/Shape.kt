23bb5481d17cc5f0a189936f9030fae2f9c0b896
손소독 <vot768@naver.com>
2022-06-04 13:13:02 +0900

    1. 테마 구성

--- app/src/main/java/com/vot/audioplayer/ui/theme/Shape.kt
+++ app/src/main/java/com/vot/audioplayer/ui/theme/Shape.kt
@@ -1,2 +1,11 @@
 package com.vot.audioplayer.ui.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shape = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp),
+)
--- app/src/main/java/com/vot/audioplayer/ui/theme/Theme.kt
+++ app/src/main/java/com/vot/audioplayer/ui/theme/Theme.kt
@@ -1,1 +1,40 @@
 package com.vot.audioplayer.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+)
+
+private val LightColorPalette = lightColors(
+    primary = Orange200,
+    primaryVariant = Blue700,
+    secondary = Orange200,
+    background = Blue700,
+    surface = Blue500,
+    onSurface = Color.White
+)
+
+@Composable
+fun AudioPlayerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shape,
+        content = content
+    )
+}
