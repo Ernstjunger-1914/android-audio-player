56f950dccc8629d0d667427b367d496681b54143
손소독 <vot768@naver.com>
2022-06-04 17:31:00 +0900

    1. MainScreen 추가 2. 권한 비허용시 권한 허용 Text 뜨도록 구현

--- app/src/main/java/com/vot/audioplayer/MainActivity.kt
+++ app/src/main/java/com/vot/audioplayer/MainActivity.kt
@@ -1,13 +1,90 @@
 package com.vot.audioplayer
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.vot.audioplayer.ui.audio.AudioViewModel
+import com.vot.audioplayer.ui.audio.MainScreen
+import com.vot.audioplayer.ui.theme.AudioPlayerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AudioPlayerTheme {
+                val permissionState = rememberPermissionState(
+                    permission = READ_EXTERNAL_STORAGE
+                )
+
+                val lifeCycleOwner = LocalLifecycleOwner.current
+
+                DisposableEffect(key1 = lifeCycleOwner) {
+                    val observer = LifecycleEventObserver { _, e ->
+                        if (e == Lifecycle.Event.ON_RESUME) {
+                            permissionState.launchPermissionRequest()
+                        }
+                    }
+                    lifeCycleOwner.lifecycle.addObserver(observer)
+
+                    onDispose {
+                        lifeCycleOwner.lifecycle.removeObserver(observer)
+                    }
+                }
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    if (permissionState.hasPermission) {
+                        val audioViewModel = viewModel(
+                            modelClass = AudioViewModel::class.java
+                        )
+
+                        val audioList = audioViewModel.audioList
+
+                        MainScreen(
+                            progress = audioViewModel.currentAudioProgress.value,
+                            onProgressChange = {
+                                audioViewModel.seekTo(it)
+                            },
+                            isAutoPlaying = audioViewModel.isAudioPlaying,
+                            audioList = audioList,
+                            currentPlayerAudio = audioViewModel.currentPlayingAudio.value,
+                            onStart = {
+                                audioViewModel.playAudio(it)
+                            },
+                            onItemClick = {
+                                audioViewModel.playAudio(it)
+                            },
+                            onNext = {
+                                audioViewModel.skipToNext()
+                            }
+                        )
+                    } else {
+                        Box(contentAlignment = Alignment.Center) {
+                            Text(text = "이 앱을 사용하려면 권한을 부여해야 합니다.")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
